2d7b8850a77246ab6074d9b37f2bd6cf66fbacda
张翼 <18011446859@163.com>
2018-03-04 19:38:19 +0800

    Create main_http.cpp

---
+++ main_http.cpp
@@ -0,0 +1,16 @@
+//
+// main_http.cpp
+// web_server
+//
+
+#include "server_http.hpp"
+#include "handler.hpp"
+
+using namespace ShiyanlouWeb;
+
+int main() {
+    // HTTP 服务运行在 12345 端口，并启用四个线程
+    Server<HTTP> server(12345, 4);
+    start_server<Server<HTTP>>(server);
+    return 0;
+}
